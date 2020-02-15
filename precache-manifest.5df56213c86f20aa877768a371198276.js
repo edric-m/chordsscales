520d18e80df7888cfef9ab52465ceb5ea6a3409f
Erica Mendoza <edjomendoza@gmail.com>
2020-02-15 12:12:47 +1100

    Updates

---
+++ precache-manifest.5df56213c86f20aa877768a371198276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14360c7426fdbe609c507ab1a725775",
+    "url": "/chordsscales/index.html"
+  },
+  {
+    "revision": "2a335ee6c53f189ced49",
+    "url": "/chordsscales/static/css/main.986a23f6.chunk.css"
+  },
+  {
+    "revision": "03b93096f0b730533830",
+    "url": "/chordsscales/static/js/2.7f57dd29.chunk.js"
+  },
+  {
+    "revision": "2a335ee6c53f189ced49",
+    "url": "/chordsscales/static/js/main.6429b9a5.chunk.js"
+  },
+  {
+    "revision": "1a18295f0701bcf650f8",
+    "url": "/chordsscales/static/js/runtime-main.be4a421b.js"
+  }
+]);
